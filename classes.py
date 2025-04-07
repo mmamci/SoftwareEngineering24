c1f757aac66b235ccb7e39e9a2329378f7cef3dd
Spaghetti-OberNub <moritzmattes6@gmail.com>
2025-04-07 10:30:11 +0200

    added a parent class called Person

--- classes.py
+++ classes.py
@@ -1,13 +1,23 @@
-class Subject:
-    def __init__(self, sex, age_years):
+class Person:
+   def __init__(self, first_name, last_name):
+      self.first_name = first_name
+      self.last_name = last_name
+
+
+class Subject(Person):
+    def __init__(self,first_name, last_name,  sex, birth_date):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.__birth_date = birth_date
+    
         self.sex = sex
-        self.age_years = age_years
 
     def estimate_max_hr(self):
         """
         See https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4124545/ for different formulas
         """
 
+        # Can't find a more accurate calculation method for max heartrate.
         if self.sex == "male":
           max_hr_bpm =  223 - 0.9 * self.age_years
         elif self.sex == "female":
@@ -17,7 +27,7 @@ class Subject:
           max_hr_bpm  = input("Enter maximum heart rate:")
         return int(max_hr_bpm)
 
-class Supervisor:
+class Supervisor(Person):
     def __init__(self, first_name, last_name):
         self.first_name = first_name
         self.last_name = last_name
