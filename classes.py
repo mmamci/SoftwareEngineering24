1fdfb39ff035e5fb77cc6d591bc4bf4d8db4f25c
Spaghetti-OberNub <moritzmattes6@gmail.com>
2025-04-11 09:27:50 +0200

    Aufgabe 6.2 and Aufgabe 8: - removed experiments.py, my_function.py and tests.py - adjusted test.py for the new classes - added automatic age calculation - added post method - removed uneccesary member variables

--- classes.py
+++ classes.py
@@ -1,23 +1,43 @@
 from datetime import datetime
+from random import randint
+import sys
+import requests
+import json
 
 class Person:
-   def __init__(self, first_name, last_name):
+    def __init__(self, first_name, last_name):
       self.first_name = first_name
       self.last_name = last_name
+      self.id = self.id = last_name[0:3]+first_name[0:3]
 
+    def post(self):
+      ## Creata a new person
+      # Define the URL of the API
+      url = "http://127.0.0.1:5000/person/"
+
+      # added parameter default=str as a quick and dirty fix to save datetime objects
+      data_json = json.dumps(self.__dict__, default=str)
+
+      # Send a POST request to the API
+      response = requests.post(url, data=data_json)
+
+    
+
+      # Print the response from the server
+      print(response.headers['Location'])
+      print(response.text)
 
 class Subject(Person):
     def __init__(self,first_name : str, last_name : str,  sex : str, birth_date : str):
         super().__init__(first_name,last_name)
   
-        self.first_name = first_name
-        self.last_name = last_name
-
         try:
           self.__birth_date = datetime.fromisoformat(birth_date)
         except Exception as e:
            print(e)
+           sys.exit()
     
+        self.age = datetime.now().year - self.__birth_date.year - (0 if (self.__birth_date.month, self.__birth_date.day)  < (datetime.now().month, datetime.now().day) else 1)
         self.sex = sex
 
     def estimate_max_hr(self):
@@ -25,24 +45,25 @@ class Subject(Person):
         See https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4124545/ for different formulas
         """
 
-        # Can't find a more accurate calculation method for max heartrate.
+        # Can't find a calculation method which uses the exact birthday
         if self.sex == "male":
-          max_hr_bpm =  223 - 0.9 * self.age_years
+          max_hr_bpm =  223 - 0.9 * self.age
         elif self.sex == "female":
-          max_hr_bpm = 226 - 1.0 *  self.age_years
+          max_hr_bpm = 226 - 1.0 *  self.age
         else:
           # der input() öffnet ein Eingabefenster für den Nutzer und speichert die Eingabe
           max_hr_bpm  = input("Enter maximum heart rate:")
         return int(max_hr_bpm)
 
+
 class Supervisor(Person):
     def __init__(self, first_name, last_name):
-        self.first_name = first_name
-        self.last_name = last_name
+        super().__init__(first_name, last_name)
+
 
 class Experiment:
-    def __init__(self, name, date):
-        self.name = name
+    def __init__(self, date, supervisor, subject, id = randint(0,100000)):
         self.date = date
-
-subject = Subject("Moritz", "Mattes", "Male", "2004-12-02")
+        self.id = id
+        self.supervisor = supervisor
+        self.subject = subject
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from my_functions import estimate_max_hr
+from classes import Subject, Supervisor, Experiment
 
 if __name__ == "__main__":
-    print(estimate_max_hr(50, "male"))
+    supervisor = Supervisor("Moritz", "Mattes")
+    subject = Subject("Linus","Torvalds", "male", "1969-04-28")
+    experiment = Experiment("27.03.2025", supervisor, subject)
 
-
+    subject.post()
